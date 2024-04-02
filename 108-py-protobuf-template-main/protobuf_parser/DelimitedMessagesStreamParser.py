5d2584decfe462fae5e4c7e7bcef924a4c7450ab
Hippo Maru <hippomaru@ya.ru>
2024-04-02 23:41:57 +0300

    Happy BirthDay to me

--- 108-py-protobuf-template-main/protobuf_parser/DelimitedMessagesStreamParser.py
+++ 108-py-protobuf-template-main/protobuf_parser/DelimitedMessagesStreamParser.py
@@ -1,22 +1,30 @@
-from helpers import parse_delimited_message
 from protobuf_parser.helpers import parseDelimited
 from google.protobuf.internal.decoder import _DecodeVarint32
-
-
 from typing import TypeVar, Type
-
+ 
 T = TypeVar('T')
-
+ 
+ 
 class DelimitedMessagesStreamParser:
     def __init__(self, cls: Type[T]) -> None:
         self.cls = cls
-
-    def parse(self, data: bytes) -> list[Type[T]]:
+        self.buf: bytes = b''
+ 
+    def parse(self, data):
         messages = []
-        pos = 0
-        while pos < len(data):
-            message, bytes_consumed = parse_delimited_message(data, self.cls, pos)
-            messages.append(message)
-            pos += bytes_consumed
 
+        try:
+            self.buf += data
+        except TypeError:
+            return []
+
+        while self.buf:
+            message, ind = parseDelimited(self.buf, self.cls)
+            if message:
+                messages.append(message)
+            elif ind == 0:
+                break
+ 
+            self.buf = self.buf[ind:]
+ 
         return messages
--- 108-py-protobuf-template-main/protobuf_parser/helpers.py
+++ 108-py-protobuf-template-main/protobuf_parser/helpers.py
@@ -1,28 +1,17 @@
-from typing import TypeVar, Any, Type
 from google.protobuf.internal.decoder import _DecodeVarint32
+from google.protobuf.message import DecodeError
+from typing import TypeVar, Any, Type
 
 T = TypeVar('T')
 
-def parse_delimited_message(data: bytes, cls: Type[T], start: int = 0) -> tuple[T, int]:
-    """
-    \brief Расшифровывает сообщение,
-    предваренное длиной из массива байтов.
-        
-    \param data Массив данных.
-    \param cls Тип сообщения.
-
-    \return Возвращает кортеж из
-    расшифрованного сообщения и количество байт,
-    которое потребовалось для расшифровки.
-    """
-    message_size, pos = _DecodeVarint32(data, start)
-    if message_size < 0:
-        raise ValueError("Invalid message size")
-
-    end_pos = pos + message_size
-    if end_pos > len(data):
-        raise ValueError("Incomplete message data")
-
-    message = cls()
-    message.ParseFromString(data[pos:end_pos])
-    return message, end_pos - start
+def parseDelimited(data: Any, cls: Type[T]) -> tuple[Type[T], int]:
+    if data:
+        size, ind = _DecodeVarint32(data, 0)
+        if size + ind <= len(data): 
+            message = cls()
+            try:
+                message.ParseFromString(data[ind:ind+size])
+            except DecodeError as e:
+                raise ValueError(str(e))
+            return message, size + ind
+    return None, 0
